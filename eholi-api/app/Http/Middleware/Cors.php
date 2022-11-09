f8f94b55871e9d614d44e2c62f136f5932225321
yuri2397 <diawlemor97@gmail.com>
2022-11-09 00:28:03 +0000

    Add cors in api.php

--- eholi-api/app/Http/Middleware/Cors.php
+++ eholi-api/app/Http/Middleware/Cors.php
@@ -16,6 +16,8 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
